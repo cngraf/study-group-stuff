180c0d97ac1d9cb941c05102aa60ee3bfebd3d2e
cngraf <christopher.n.graf@gmail.com>
2016-04-11 20:12:01 -0500

    add solution

--- stack.rb
+++ stack.rb
@@ -1,3 +1,26 @@
 class Stack
-  # Your code here!
+  def initialize(head = nil)
+    @head = head
+    @tail = nil
+  end
+
+  def push(thing)
+    @tail = self.dup
+    @head = thing
+  end
+
+  def pop
+    return nil unless @head
+    temp_head = @head
+    if @tail.nil?
+      @head = nil
+    else
+      @tail = nil unless (@head = @tail.pop)
+    end
+    temp_head
+  end
+
+  def empty?
+    @head.nil? && @tail.nil?
+  end
 end
