1dc3e1f5b7b6e113e3ebcd62a5d01e003f6f2c2d
cngraf <christopher.n.graf@gmail.com>
2016-04-11 20:15:06 -0500

    remove redundant guard

--- stack_spec.rb
+++ stack_spec.rb
@@ -17,12 +17,26 @@ RSpec.describe Stack do
   it 'retrieves items in last-in-first-out order' do
     stack = Stack.new
     stack.push(1)
+    puts stack.inspect
     stack.push(2)
+    puts stack.inspect
     stack.push(3)
+    puts stack.inspect
     expect(stack.pop).to eq 3
+    puts stack.inspect
+
     stack.push(4)
+    puts stack.inspect
+
     expect(stack.pop).to eq 4
+    puts stack.inspect
+
     expect(stack.pop).to eq 2
+    puts stack.inspect
+
     expect(stack.pop).to eq 1
+    puts stack.inspect
+
+    expect(stack.empty?).to be true
   end
 end
